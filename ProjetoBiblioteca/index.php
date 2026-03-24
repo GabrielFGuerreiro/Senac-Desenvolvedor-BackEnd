36ec26b183c8b3d087e86f9fb0f40e3870d5d5df
GabrielFGuerreiro <gaferg2004@gmail.com>
2026-03-23 21:55:20 -0300

    Começo projeto biblioteca

---
+++ ProjetoBiblioteca/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto Biblioteca</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+    include("menu.php");
+    ?>
+    <div class="content">
+        <h1>Biblioteca Digital Municipal</h1>
+        <p>A Biblioteca Digital Municipal tem como missão promover o acesso ao conhecimento, à 
+            cultura e a à educação por meio de um acervo digital acessível a toda a comunidade.</p>
+        <br>
+        <p>Nosso sistema permite consultar obras literárias, acessar livros digitais e incentivar
+        a leitura através da tecnologia.
+            Este ambiente foi desenvolvido para facilitar o gerenciamento do acervo blibliográfico
+        e aproximar leitores do universo literário.</p>
+    </div>
+    <img src="https://www.uninter.com/noticias/wp-content/uploads/2020/12/noticias_tablet-1632909_1920-1024x680.jpg" style="width:100%;border-radius: 12px;">
+
+</body>
+</html>
---
+++ ProjetoBiblioteca/menu.php
@@ -0,0 +1,7 @@
+<div class="sidebar">
+    <h2>Biblioteca</h2>
+    <a href="index.php">Início</a>
+    <a href="index.php">Acervo</a>
+    <a href="index.php">Cadastrar Livro</a>
+    <a href="index.php">Início</a> 
+</div>
