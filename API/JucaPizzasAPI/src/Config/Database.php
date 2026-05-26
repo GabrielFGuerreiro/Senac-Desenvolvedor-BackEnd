f343a15d7a4e4293d429faf90f377202a2b5f35a
GabrielFGuerreiro <gaferg2004@gmail.com>
2026-05-25 22:06:59 -0300

    Refatoração do projeto, adicionado composer.

--- API/JucaPizzasAPI/src/Config/Database.php
+++ API/JucaPizzasAPI/src/Config/Database.php
@@ -1,5 +1,6 @@
 <?php
 
+namespace Gfg\Jucapizzasapi\Config;
 class Database {
 
     private $host = "localhost";
@@ -17,13 +18,13 @@ class Database {
         { 
             //DSN (Data Source Name) é uma string que contém as informações necessárias para se conectar ao banco de dados.
             $dsn = "mysql:host={$this->host};port={$this->port};dbname={$this->db_name};charset=utf8";
-            $this->conn = new PDO($dsn, $this->username, $this->password); //Cria uma nova conexão PDO usando o DSN e as credenciais fornecidas.
+            $this->conn = new \PDO($dsn, $this->username, $this->password); //Cria uma nova conexão PDO usando o DSN e as credenciais fornecidas.
 
             //Define o modo de erro do PDO para exceção
             //Isso faz com que o PDO lance exceções em caso de erros, facilitando o tratamento
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         }
-        catch(Exception $e) //Em caso de erro na conexão, exibe a messagem de erro.
+        catch(\Exception $e) //Em caso de erro na conexão, exibe a messagem de erro.
         { 
             echo "Erro de conexão: " . $e->getMessage();
         }
